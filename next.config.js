aba33a7d6ac4f6ebf92154e41480cb313d0725bb
OwenKim <kimdu0301@gmail.com>
2025-08-04 17:23:18 +0900

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "export",
-//   trailingSlash:true,
-//   images: {
-//     unoptimized: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
