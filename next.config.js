7c4f2df41db74987fd92ea9f5224216d74854fbd
OwenKim <kimdu0301@gmail.com>
2025-08-04 17:20:15 +0900

    replace next.config.ts to .js

---
+++ next.config.js
@@ -0,0 +1,29 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   trailingSlash:true,
+//   images: {
+//     unoptimized: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
